db40044532cf1330cc6da9d619fe4991fa3a5f50
Jose Cordoba <jmcordoba@universidadean.edu.co>
2019-05-08 20:33:20 -0500

    Creada la prueba para contar vocales

---
+++ funciones.py
@@ -0,0 +1,13 @@
+def contar_vocales(cadena):
+    """
+    (str) -> int
+
+    Cuenta las vocales en una cadena
+
+    >>> contar_vocales('hola')
+    2
+
+    :param cadena: la cadena a evaluar
+    :return: int el numero de vocales en la cadena
+    """
+    pass
---
+++ test_contar_vocales.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from funciones import *
+
+class TestContar_vocales(TestCase):
+
+    def test_contar_vocales(self):
+        # Casos estandar
+        dado = 'hola mundo'
+        espero = 4
+        real = contar_vocales(dado)
+        self.assertEquals(espero, real)
+
+        dado = 'UniEMPREsaRIAL'
+        espero = 7
+        real = contar_vocales(dado)
+        self.assertEquals(espero, real)
+
+        # Casos Extremos
+
+        # Casos Excepcionales
